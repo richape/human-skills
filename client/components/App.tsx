630944c07e73fbb76bd4e06cd12b3e7c18c5aeb2
Richard <richard.hape@devacademy.co.nz>
2024-01-15 09:45:48 +1300

    15/01 initial commit

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,9 @@
+import Home from './Home'
+
 function App() {
   return (
     <>
-      <div className="bg-purple-700 text-yellow-500 p-5 text-xl font-bold font-sans text-center">
-        <h2>Welcome to the</h2>
-        <h1 className="text-3xl font-extrabold">Human Skills</h1>
-        <h2>reflections App</h2>
-      </div>
+      <Home />
     </>
   )
 }
---
+++ client/components/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <div className="bg-purple-700 text-yellow-500 p-5 text-xl font-bold font-sans text-center">
+        <h2>Welcome to the</h2>
+        <h1 className="text-3xl font-extrabold">Human Skills</h1>
+        <h2>reflections App</h2>
+      </div>
+    </>
+  )
+}
+
+export default Home
