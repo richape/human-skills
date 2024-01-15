88a5d5050c2c659a30afa4435611b5f849cb551d
Richard <richard.hape@devacademy.co.nz>
2024-01-16 12:12:16 +1300

    added to Schedule.tsx to work on later & added image to Logo.tsx

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -2,9 +2,9 @@ function Logo() {
   return (
     <div className="flex items-center justify-center p-8">
       <img
-        // src="/images/logo.jpg"
+        src="/images/logo.png"
         alt="Dev Academy Logo"
-        style={{ width: '100px', height: '100px' }}
+        style={{ width: '25%' }}
       ></img>
     </div>
   )
--- client/components/Schedule.tsx
+++ client/components/Schedule.tsx
@@ -1,5 +1,55 @@
+import { useState } from 'react'
+
+const TestSchedule: React.FC = () => {
+  const [phaseOne, setPhaseOne] = useState<string | null>(null)
+  const [phaseTwo, setPhaseTwo] = useState<string | null>(null)
+  const [phaseThree, setPhaseThree] = useState<string | null>(null)
+
+  const phaseOneOptions = ['Week 1', 'Week 2', 'Week 3']
+  const phaseTwoOptions = ['Week 4', 'Week 5', 'Week 6']
+  const phaseThreeOptions = ['Week 7', 'Week 8', 'Week 9']
+}
+
 function Schedule() {
-  return <div></div>
+  return (
+  <div>
+    <div>
+      <label>Phase One</label>
+      <select onChange={(e) => setPhaseOne(e.target.value)} value={phaseOne || ''}>
+        <option value="">Select Week</option>
+        {phaseOneOptions.map((week, index) => (
+          <option key={index} value={week}>
+            {week}
+          </option>
+        ))}
+      </select>
+    </div>
+  
+    <div>
+      <span>Phase Two</span>
+      <select onChange={(e) => setPhaseTwo(e.target.value)} value={phaseTwo || ''}>
+        <option value="">Select Week</option>
+        {phaseTwoOptions.map((week, index) => (
+          <option key={index} value={week}>
+            {week}
+          </option>
+        ))}
+      </select>
+    </div>
+  
+    <div>
+      <span>Phase Three</label>
+      <select onChange={(e) => setPhaseThree(e.target.value)} value={phaseThree || ''}>
+        <option value="">Select Week</option>
+        {phaseThreeOptions.map((week, index) => (
+          <option key={index} value={week}>
+            {week}
+          </option>
+        ))}
+      </select>
+    </div>
+  </div>
+  )
 }
 
 export default Schedule
