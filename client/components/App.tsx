c66a81b4179bfc158dd2dc3fc15b555026e0112d
Richard <richard.hape@devacademy.co.nz>
2024-01-15 14:41:01 +1300

    add brand logo and organise components

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,12 @@
+import Logo from './Logo'
 import Footer from './Footer'
-import Home from './Home'
 
 function App() {
   return (
-    <>
-      <Home />
+    <div className="bg-purple-700">
+      <Logo />
       <Footer />
-    </>
+    </div>
   )
 }
 
---
+++ client/components/Logo.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div className="flex items-center justify-center">
+      <img src="/images/logo.jpg" alt="Dev-Academy-Logo"></img>
+    </div>
+  )
+}
+
+export default Logo
