66308aa2eca6f9e79f8099ec681ca1e9c0c58be6
Richard <richard.hape@devacademy.co.nz>
2024-01-19 11:44:55 +1300

    working on Schedule.tsx and Dropdown menu

--- client/components/Schedule.tsx
+++ client/components/Schedule.tsx
@@ -1,5 +1,15 @@
 function Schedule() {
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>Phase One</li>
+          <li>Phase Two</li>
+          <li>Phase Three</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Schedule
