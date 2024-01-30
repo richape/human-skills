bb91753e60b4a031e730d435dc35f0aeef9e0b18
Richard <richard.hape@devacademy.co.nz>
2024-01-31 10:45:41 +1300

    work on buttons

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -9,7 +9,9 @@ function Login({ className }: LoginProps) {
         className="p-3.5 rounded-md drop-shadow-xl"
         style={{ backgroundColor: '#7E51B2' }}
       >
-        <button className="text-yellow-500 btn">login</button>
+        <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
+          login
+        </button>
       </div>
     </div>
   )
--- client/components/Register.tsx
+++ client/components/Register.tsx
@@ -7,7 +7,9 @@ function Register() {
         className="p-3.5 rounded-md drop-shadow-xl"
         style={{ backgroundColor: '#7E51B2' }}
       >
-        <button className="text-yellow-500 px-4 btn">sign up</button>
+        <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
+          sign up
+        </button>
       </div>
     </div>
   )
