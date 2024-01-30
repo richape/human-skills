93835095e41cf4d9e563f77ff95cadcb1e56c40f
Richard <richard.hape@devacademy.co.nz>
2024-01-31 12:05:54 +1300

    trying to build a form using react-hook-form

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -6,7 +6,8 @@ function Login({ className }: LoginProps) {
   return (
     <div className={className}>
       <div
-        className="p-3.5 rounded-md drop-shadow-xl"
+        className="rounded-md drop-shadow-xl"
+        // p-3.5
         style={{ backgroundColor: '#7E51B2' }}
       >
         <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
--- client/components/Register.tsx
+++ client/components/Register.tsx
@@ -4,7 +4,8 @@ function Register() {
   return (
     <div>
       <div
-        className="p-3.5 rounded-md drop-shadow-xl"
+        className="rounded-md drop-shadow-xl"
+        // p-3.5
         style={{ backgroundColor: '#7E51B2' }}
       >
         <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
