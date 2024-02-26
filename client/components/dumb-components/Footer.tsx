ebae4a2facbad303803442f7c548dca6ac0910ae
Richard <richard.hape@devacademy.co.nz>
2024-02-27 11:43:15 +1300

    working on components

--- client/components/dumb-components/Footer.tsx
+++ client/components/dumb-components/Footer.tsx
@@ -1,14 +1,16 @@
 function Footer() {
   return (
-    <div className="bottom-0 left-0 w-full">
-      <div className="flex items-center justify-center p-5 h-full">
-        <img
-          src="/images/footer.png"
-          alt="Dev Academy Footer"
-          style={{ width: '100%' }}
-        ></img>
+    <footer>
+      <div className="bottom-0 left-0 w-full">
+        <div className="flex items-center justify-center p-5 h-full">
+          <img
+            src="/images/footer.png"
+            alt="Dev Academy Footer"
+            style={{ width: '100%' }}
+          ></img>
+        </div>
       </div>
-    </div>
+    </footer>
   )
 }
 
--- client/components/dumb-components/Logo.tsx
+++ client/components/dumb-components/Logo.tsx
@@ -4,13 +4,15 @@ interface LogoProps {
 
 function Logo({ className }: LogoProps) {
   return (
-    <div className={`flex items-center justify-center p-5 ${className}`}>
-      <img
-        src="/images/logo.png"
-        alt="Dev Academy Logo"
-        style={{ width: '20%' }}
-      ></img>
-    </div>
+    <>
+      <div className={`flex items-center justify-center p-5 ${className}`}>
+        <img
+          src="/images/logo.png"
+          alt="Dev Academy Logo"
+          style={{ width: '20%' }}
+        ></img>
+      </div>
+    </>
   )
 }
 
