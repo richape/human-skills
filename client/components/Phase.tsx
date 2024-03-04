214433b7c2d9cc3d120c71cb969c7e6bb9c49390
Richard <richard.hape@devacademy.co.nz>
2024-03-04 14:25:12 +1300

    working on routes.tsx

--- client/components/Phase.tsx
+++ client/components/Phase.tsx
@@ -1,4 +1,12 @@
 function Phase() {
-  return <></>
+  return (
+    <>
+      <ul>
+        <li>Phase 1</li>
+        <li>Phase 2</li>
+        <li>Phase 3</li>
+      </ul>
+    </>
+  )
 }
 export default Phase
--- client/routes.tsx
+++ client/routes.tsx
@@ -2,10 +2,12 @@ import { Route, createRoutesFromElements } from 'react-router-dom'
 import App from './components/App'
 import Home from './components/nl-components/Home'
 import Register from './components/Register'
+import Phase from './components/Phase'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
     <Route index element={<Register />} />
+    <Route index element={<Phase />} />
   </Route>,
 )
