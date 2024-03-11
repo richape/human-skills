d4bc8b703b38683a1826fe88df0f0677c3c2a02c
Richard <richard.hape@devacademy.co.nz>
2024-03-12 11:02:46 +1300

    learning to explain samples of code

--- client/components/BookingCalendar.tsx
+++ client/components/BookingCalendar.tsx
@@ -5,13 +5,34 @@ type ValuePiece = Date | null
 
 type Value = ValuePiece | [ValuePiece, ValuePiece]
 
+/**
+ * A component to display a calendar for booking a date range.
+ */
 function BookingCalendar() {
+  /**
+   * The selected date range in the calendar. Can be a single date or a range of
+   * two dates. Initially set to the current date.
+   */
   const [value, onChange] = useState<Value>(new Date())
+
+  /**
+   * Render the component.
+   */
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        /**
+         * Callback function to handle when the selected date range changes.
+         */
+        onChange={onChange}
+        /**
+         * The selected date range to display in the calendar.
+         */
+        value={value}
+      />
     </div>
   )
 }
 
 export default BookingCalendar
+// Learning to use Codeium for best practice
