7270f678aff0621b230ae608040d05167c62e6d8
Richard <richard.hape@devacademy.co.nz>
2024-03-13 14:35:00 +1300

    working on code structure

--- client/components/Register.tsx
+++ client/components/Register.tsx
@@ -1,19 +1,38 @@
 // Need to work on Auth0 form and routes
+import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useNavigate } from 'react-router-dom'
 
-function Register() {
-  return (
-    <div>
-      <div
-        className="rounded-md drop-shadow-xl"
-        // p-3.5
-        style={{ backgroundColor: '#7E51B2' }}
-      >
-        <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
-          sign up
-        </button>
-      </div>
-    </div>
-  )
+export default function Register() {
+  const { user, getAccessTokenSilently } = useAuth0()
+  const userDb = useUser()
+  const navigate = useNavigate()
+  const initialFormData = {
+    Full_name: '',
+    Email: '',
+    Cohort: '',
+    Password: '',
+    'Confirm Password': '',
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  useEffect(() => {
+    if (userDb.data) navigate('/')
+  }, [userDb.data, navigate])
 }
 
-export default Register
+// function Register() {
+return (
+  <div>
+    <div
+      className="rounded-md drop-shadow-xl"
+      // p-3.5
+      style={{ backgroundColor: '#7E51B2' }}
+    >
+      <button className="text-yellow-500 py-2 px-5 rounded-lg text-xl font-bold bg-opacity-75 cursor-pointer">
+        sign up
+      </button>
+    </div>
+  </div>
+)
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,15 +1,21 @@
-import { Route, createRoutesFromElements } from 'react-router-dom'
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from 'react-router-dom'
 import App from './components/App'
 import Home from './components/nl-components/Home'
 import Register from './components/Register'
 import Phase from './components/Phase'
 import AppIntro from './components/nl-components/AppIntro'
 
-export const routes = createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<Home />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/app-intro" element={<AppIntro />} />
-    <Route path="/phase" element={<Phase />} />
-  </Route>,
+export const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/app-intro" element={<AppIntro />} />
+      <Route path="/phase" element={<Phase />} />
+    </Route>,
+  ),
 )
